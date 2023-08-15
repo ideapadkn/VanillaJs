dd1ca58820a029888c7db7235b8be371c2d5e2f4
Kamron or Камрон <kamrush6429@gmail.com>
2023-08-15 15:23:45 +0500

    finished component products

--- components/Products/products.js
+++ components/Products/products.js
@@ -3,22 +3,24 @@ class Products {
     let htmlCatalog = "";
     CATALOG.forEach(({ id, name, price, img }) => {
       htmlCatalog += `
-        <li>
-          <span>${name}</span>
-          <img src="${img}" />
-          <span>${price}</span>
-          <button>Add to Cart</button>
+        <li class="products-element">
+          <span class="products-element__name">${name}</span>
+          <img class="products-element__img" src="${img}" />
+          <span class="products-element__price">
+            ${price.toLocaleString()} USD
+          </span>
+          <button class="products-element__btn">Add to Cart</button>
         </li>
       `;
     });
 
     const html = `
-      <ul>
+      <ul class="products-container">
         ${htmlCatalog}
       </ul>
     `;
 
-    ROOT_PRODUCTS.innerHTML = html
+    ROOT_PRODUCTS.innerHTML = html;
   }
 }
 
